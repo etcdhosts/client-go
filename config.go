a8066b2e421583347c395c48a37b64bef097f322
mritd <mritd@linux.com>
2020-12-17 18:30:32 +0800

    feat(config): add struct tag
    add struct tag
    
    Signed-off-by: mritd <mritd@linux.com>

--- config.go
+++ config.go
@@ -5,13 +5,13 @@ import (
 )
 
 type Config struct {
-	CA          string        `json:"ca" yaml:"ca"`
-	Cert        string        `json:"cert" yaml:"cert"`
-	Key         string        `json:"key" yaml:"key"`
-	UserName    string        `json:"user_name" yaml:"user_name"`
-	Password    string        `json:"password" yaml:"password"`
-	Endpoints   []string      `json:"endpoints" yaml:"endpoints"`
-	HostsKey    string        `json:"hosts_key" yaml:"hosts_key"`
-	DialTimeout time.Duration `json:"dial_timeout" yaml:"dial_timeout"`
-	ReqTimeout  time.Duration `json:"req_timeout" yaml:"req_timeout"`
+	CA          string        `json:"ca,omitempty" yaml:"ca,omitempty"`
+	Cert        string        `json:"cert,omitempty" yaml:"cert,omitempty"`
+	Key         string        `json:"key,omitempty" yaml:"key,omitempty"`
+	UserName    string        `json:"user_name,omitempty" yaml:"user_name,omitempty"`
+	Password    string        `json:"password,omitempty" yaml:"password,omitempty"`
+	Endpoints   []string      `json:"endpoints,omitempty" yaml:"endpoints,omitempty"`
+	HostsKey    string        `json:"hosts_key,omitempty" yaml:"hosts_key,omitempty"`
+	DialTimeout time.Duration `json:"dial_timeout,omitempty" yaml:"dial_timeout,omitempty"`
+	ReqTimeout  time.Duration `json:"req_timeout,omitempty" yaml:"req_timeout,omitempty"`
 }
